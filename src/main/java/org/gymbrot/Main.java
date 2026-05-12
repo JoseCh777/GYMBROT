8f210b784110a24b7ec8a7659c1a9c2fd2faeaff
Jose Chinchia <jachinchia@unicesar.edu.co>
2026-05-12 17:54:29 -0500

    "feat: Base de datos conectada"

--- src/main/java/org/gymbrot/Main.java
+++ src/main/java/org/gymbrot/Main.java
@@ -2,9 +2,11 @@ package org.gymbrot;
 
 import java.sql.Connection;
 
-public static void main(String[] args) {
-    Connection conn = util.DatabaseConnection.getInstance();
-    if (conn != null) {
-        System.out.println("¡Conexión OK!");
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = util.DatabaseConnection.getInstance();
+        if (conn != null) {
+            System.out.println("¡Conexión OK!");
+        }
     }
 }
