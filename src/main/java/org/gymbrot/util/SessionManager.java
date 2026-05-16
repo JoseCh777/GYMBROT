b9dea2d97fcda38b19efce9d42bb5f036778518d
carlosdospino <carlosdospino@unicesar.edu.co>
2026-05-16 17:45:16 -0500

    feat: SessionManager - métodos getAdminActual y setAdminActual

--- src/main/java/org/gymbrot/util/SessionManager.java
+++ src/main/java/org/gymbrot/util/SessionManager.java
@@ -1,5 +1,18 @@
 package org.gymbrot.util;
 
+import org.gymbrot.model.Administrador;
+
 public class SessionManager {
 
+    private static Administrador adminActual = null;
+
+    // ── GET ADMIN ACTUAL ──────────────────────────────────────────────────
+    public static Administrador getAdminActual() {
+        return adminActual;
+    }
+
+    // ── SET ADMIN ACTUAL ──────────────────────────────────────────────────
+    public static void setAdminActual(Administrador admin) {
+        adminActual = admin;
+    }
 }
