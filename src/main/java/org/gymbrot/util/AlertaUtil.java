f6630ec384fad2d4bc0e02eace576e87e01161d4
carlosdospino <carlosdospino@unicesar.edu.co>
2026-05-17 13:19:06 -0500

    feat: AlertaUtil - métodos mostrarError y mostrarExito

--- src/main/java/org/gymbrot/util/AlertaUtil.java
+++ src/main/java/org/gymbrot/util/AlertaUtil.java
@@ -1,5 +1,16 @@
 package org.gymbrot.util;
 
+import javax.swing.JOptionPane;
+
 public class AlertaUtil {
 
+    // ── MOSTRAR ERROR ─────────────────────────────────────────────────────
+    public static void mostrarError(String titulo, String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.ERROR_MESSAGE);
+    }
+
+    // ── MOSTRAR EXITO ─────────────────────────────────────────────────────
+    public static void mostrarExito(String titulo, String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.INFORMATION_MESSAGE);
+    }
 }
