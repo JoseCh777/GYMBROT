7adfe3094899419739b2f4cb9d7fe8ace6b9430a
Jose Chinchia <jachinchia@unicesar.edu.co>
2026-05-19 07:58:24 -0500

    "fix: correcion del Main"

--- src/main/java/org/gymbrot/Main.java
+++ src/main/java/org/gymbrot/Main.java
@@ -1,12 +1,24 @@
 package org.gymbrot;
 
+import org.gymbrot.util.DatabaseConnection;
+
 import java.sql.Connection;
 
 public class Main {
+
     public static void main(String[] args) {
-        Connection conn = util.DatabaseConnection.getInstance();
-        if (conn != null) {
-            System.out.println("¡Conexión OK!");
+
+        try {
+
+            Connection conn = DatabaseConnection.getInstance();
+
+            if (conn != null) {
+                System.out.println("¡Conexión OK!");
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
         }
     }
 }
